fcdd08bb8eb5e5ed9b29f3b44ea8c8443b6d20de
trimanul <ivanko150702@gmail.com>
2020-01-07 15:29:34 +0300

    Added echobot to the repository

---
+++ echobot/echobot_tg/__init__.py
@@ -0,0 +1,48 @@
+import requests
+from bot_class import *
+
+def main():
+    tgbot = bot("956850095:AAECJIwU8DsMfQ-yHTZQduEALwgv1RZmKoo")
+    new_offset = None
+    
+    while True:
+        
+        updates = tgbot.get_updates(new_offset)
+        if updates != []:
+            update = tgbot.get_latest_update(updates)
+            chat_id = tgbot.get_chat_id(update)
+            update_id = update['update_id']
+
+            check = tgbot.msg_check(update)
+
+            if check == 'text':
+                text = tgbot.get_latest_text(update)
+                tgbot.send_message(chat_id, text)
+                print("New message: " + text)
+
+            elif check == 'sticker':
+                file_id = tgbot.get_sticker_id(update)
+                tgbot.send_sticker(chat_id, file_id)
+                print("New sticker")
+
+            elif check == 'ignore':
+                print("Something not interesting")
+        
+            new_offset = update_id + 1
+        else:
+            continue
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        raise SystemExit
+
+
+
+
+
+
+
+
+
